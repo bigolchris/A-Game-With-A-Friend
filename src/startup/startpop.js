2fb1c8858597d14026cb2a631e9cbabcf7656e85
save <csanchez7073@gmail.com>
2022-09-23 16:21:31 -0700

    added simple startup functions

---
+++ src/startup/startpop.js
@@ -0,0 +1,11 @@
+document.querySelector(".btn-play").addEventListener("click", function () {
+  document.querySelector(".modal-container").style.display = "none";
+});
+
+document.querySelector(".btn-play").addEventListener("click", function () {
+  document.querySelector(".inv-container").style.display = "flex";
+});
+
+document.querySelector(".btn-play").addEventListener("click", function () {
+  document.querySelector(".placeholder").style.display = "flex";
+});
