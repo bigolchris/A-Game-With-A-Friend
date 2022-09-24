7910238dee45869fee60544478b9f9822310c246
NathanPip <107423247+NathanPip@users.noreply.github.com>
2022-09-23 19:30:29 -0700

    styles restructure

--- src/startup/startpop.js
+++ src/startup/startpop.js
@@ -1,11 +1,11 @@
 document.querySelector(".btn-play").addEventListener("click", function () {
-  document.querySelector(".modal-container").style.display = "none";
+  document.querySelector(".modal-container").remove();
 });
 
 document.querySelector(".btn-play").addEventListener("click", function () {
-  document.querySelector(".inv-container").style.display = "flex";
+  document.querySelector(".inv-container").classList.remove("hide");
 });
 
 document.querySelector(".btn-play").addEventListener("click", function () {
-  document.querySelector(".placeholder").style.display = "flex";
+  document.querySelector(".placeholder").classList.remove("hide");
 });
