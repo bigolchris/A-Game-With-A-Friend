63924422db892a4a6550ecd9a7a9a5ddbaa2370b
NathanPip <107423247+NathanPip@users.noreply.github.com>
2022-09-29 15:51:47 -0700

    forgot to push

--- src/main.js
+++ src/main.js
@@ -1,2 +1,26 @@
 import "./startup/startpop.js";
 import "./startup/gamescreen.js";
+
+//Player Stats variables
+// let health = 100;
+// let intellgience = 100;
+// let strength = 100;
+// let vitality = 100;
+// let player = {
+//   name: ''
+//   health: 100,
+//   intelligence: 200 
+//}
+
+// 2 event Listeners
+
+//click event listener
+// handle the stat changes for both buttons
+// on the stat list
+// select based on class or id name
+// change stat
+// check for max stat change
+// do not change stat if max stat change is exceeded
+
+//click event listener
+// start game
