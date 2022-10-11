494cb72e1ac4cfd66a745df5a74869a12e483f12
NathanPip <107423247+NathanPip@users.noreply.github.com>
2022-10-11 16:14:27 -0700

    main title screen style update and remove garbage in js

--- src/main.js
+++ src/main.js
@@ -16,11 +16,6 @@ let playerStats = {
 
 let points = 10;
 
-// let player = {
-//   name: "",
-//   health: 100,
-//   intelligence: 200,
-// };
 let playerName = document.querySelector(".char-input").value;
 
 let newPlayer = document.querySelector(".create-character");
@@ -38,8 +33,6 @@ engStat.innerHTML = playerStats.engineering;
 stealthStat.innerHTML = playerStats.stealth;
 
 let pointDisplay = document.querySelector(".points");
-let removePoints = document.querySelector(".add-btn");
-let replacePoints = document.querySelector(".sub-btn");
 
 pointDisplay.innerHTML = points;
 
@@ -47,9 +40,6 @@ let subGroup = document.querySelector(".btn-group");
 
 subGroup.addEventListener("click", (e) => {
   console.log(e.target.classList.contains("str-sub"));
-
-  let fish = e.target.className;
-  console.log(fish);
 
   let statChange = 0;
   const el = e.target;
@@ -74,23 +64,6 @@ subGroup.addEventListener("click", (e) => {
   playerStats[stat] += statChange;
   statEl.innerText = playerStats[stat];
 
-  // if (el.classList.contains("intel")) {
-  //   intelligence += statChange;
-  //   intelStat.innerHTML = intelligence;
-  // } else if (el.classList.contains("str")) {
-  //   strength += statChange;
-  //   strengthStat.innerHTML = strength;
-  // } else if (el.classList.contains("ins")) {
-  //   insight += statChange;
-  //   insightStat.innerHTML = insight;
-  // } else if (el.classList.contains("eng")) {
-  //   engineering += statChange;
-  //   engStat.innerHTML = engineering;
-  // } else if (el.classList.contains("stealth")) {
-  //   stealth += statChange;
-  //   stealthStat.innerHTML = stealth;
-  // }
-
   points -= statChange;
   pointDisplay.innerHTML = points;
   console.log(points);
@@ -102,28 +75,3 @@ newPlayer.addEventListener("click", () => {
   players.push(playerStats);
   console.log(players);
 });
-
-// let sub = 1;
-
-// // 2 event Listeners
-// document.querySelector(".add-btn").addEventListener("click", function () {
-//   pointsSub.map((elem) => {
-//     elem.innerHTML = parseInt(elem.innerHTML) - sub;
-//   });
-// });
-// document.querySelector(".sub-btn").addEventListener("click", function () {
-//   pointsSub.map((elem) => {
-//     elem.innerHTML = parseInt(elem.innerHTML) + sub;
-//   });
-// });
-// console.log(points);
-//click event listener
-// handle the stat changes for both buttons
-// on the stat list
-// select based on class or id name
-// change stat
-// check for max stat change
-// do not change stat if max stat change is exceeded
-
-//click event listener
-// start game
